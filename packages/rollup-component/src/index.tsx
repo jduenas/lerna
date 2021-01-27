ee04757767f3d7dfa60d47a2d9d28e502fa7ecd9
Jose Duenas <jduenas@outlook.com>
2021-01-26 20:56:51 -0500

    Testing multiple class application from css modules

--- packages/rollup-component/src/index.tsx
+++ packages/rollup-component/src/index.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react'
 import style from './styles.module.css'
 
 function RollupComponent() {
-  return <div className={style.test}>Rollup Component Successfuly!!</div>
+  const [state, setState] = useState({
+    clicked: false
+  })
+
+  let currentState = ''
+  if (state.clicked) {
+    currentState = style.clicked
+  }
+
+  return (
+    <button
+      onClick={() => setState({ clicked: !state.clicked })}
+      className={`${style.container} ${currentState}`}
+    >
+      {`Rollup Component Successfuly: ${
+        state.clicked ? 'CLICKED' : 'NOT CLICKED'
+      }`}
+    </button>
+  )
 }
 
 export default RollupComponent
